126637ef83b7676b44bf6df234f26dd737bc806b
sanekua <sasha-kuzmyk@mail.ru>
2019-06-22 21:25:42 +0300

    first step

--- adv_scrapy/adv_scrapy/spiders/web_scraper.py
+++ adv_scrapy/adv_scrapy/spiders/web_scraper.py
@@ -9,10 +9,6 @@ def parse(self, response):
     for respons in all_items:
         product_name = respons.css('.s-access-title').css('::text').extract()
         product_corp = respons.css('.a-color-secondary+ .a-color-secondary').css('::text').extract()
-        product_price = respons.css('.a-color-base , .sx-price-whole').css('::text').extract()
-        product_image = respons.css('.cfMarker::attr(src)').extract()
-        items['product_name'] = product_name
-        items['product_corp'] = product_corp
         items['product_price'] = (re.search(r'\d+', str(product_price)).group(0)).split()
         items['product_image'] = product_image
         yield items
